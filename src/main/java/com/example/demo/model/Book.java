4ae88cbe32e1db6b622287f942a857aa5434b35b
Mhoanganh207 <mhoanganh05@gmail.com>
2023-09-02 22:03:18 +0700

    update

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,9 +1,15 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,7 +32,9 @@ public class Book {
 	private String category;
 	@Column(name="description",length = 1000)
 	private String description;
-	@ManyToOne
-	private Order order;
+	
+	@ManyToMany(mappedBy = "book_list", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Order> order = new ArrayList<Order>();
 
 }
--- src/main/java/com/example/demo/model/Order.java
+++ src/main/java/com/example/demo/model/Order.java
@@ -2,12 +2,15 @@ package com.example.demo.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,12 +24,18 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@ManyToOne
 	
+	@ManyToOne
 	private User user;
 	
-	@OneToMany(mappedBy = "order")
+	@ManyToMany
+	@JoinTable(
+	        name = "Order_Book", 
+	        joinColumns = { @JoinColumn(name = "order_id") }, 
+	        inverseJoinColumns = { @JoinColumn(name = "book_id") }
+	    )
 	private List<Book> book_list = new ArrayList<Book>();
+	
 	private String address;
 	private String phonenumber;
 	private double totalprice;
